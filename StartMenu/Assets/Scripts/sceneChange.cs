1b24701e68f30d081483e43b2119ac20df256da0
Denis <denis.nguyen2012@googlemail.com>
2017-10-12 14:17:07 +0200

    hochgeladen für abdu

---
+++ StartMenu/Assets/Scripts/sceneChange.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class sceneChange : MonoBehaviour {
+
+
+	
+	// Update is called once per frame
+	public  void sceneChangeTo (string change) {
+		SceneManager.LoadScene(change);
+	}
+}
